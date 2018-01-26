0254cc181b33e0bbe2cc5fd73c89c7d27bae9c79
Unknown <mathie.bertholet@iut-valence.fr>
2018-01-26 10:53:35 +0100

    Client pop changement synthase

--- ClietnSMTP/src/Pop.java
+++ ClietnSMTP/src/Pop.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -8,92 +9,45 @@ class Pop {
     String user;
     String pass;
 
-    Pop(String zserver,
-            String zuser, String zpass) {
+    Pop(String zserver,String zuser, String zpass) {
         server = zserver;
         user = zuser;
         pass = zpass;
     }
 
     void lit() {
-        PrintWr iter to;
+        PrintWriter to;
         BufferedReader from;
         String str, title, msg;
         Vector v = new Vector();
         try {
-            Socket socket = n
-            ew Socket
-            (In etAddress
-            .g etByName(server
-            )
-,110)
-; 
-        to = new PrintWriter(
-                    new BufferedWriter(
-                            new OutputStreamWriter(
-                                    socket.getOutputStream())), tru
-e
-            ); 
-        from = new BufferedReader(
-                    new InputStreamReader(
-                            socket.getInpu
-            tStream()
-            ))
-; 
-        while (!(from.readLine())
-                    .startsWith("+OK"
-                    ));
-            to.println("USER "
-                    + user + "\r");
-            while (!(from.readLine())
-                    .startsWith("+OK"
-                    ));
+            Socket socket = new Socket(InetAddress.getByName(server), 110);
+            to = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+            from = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while (!(from.readLine()).startsWith("+OK"));
+            to.println("USER " + user + "\r");
+            while (!(from.readLine()).startsWith("+OK"));
             to.println("PASS " + pass + "\r");
-            while (!(from.readLine())
-                    .startsWith("+OK"
-                    ));
-            http://www.nicola
-            sjean.
-c
-
-            om
-Nicolas JEA
-            N to
-            .println("LIST\r");
-            while (!(from.readLine())
-                    .startsWith("+OK"
-                    ));
+            while (!(from.readLine()).startsWith("+OK"));
+            to.println("LIST\r");
+            while (!(from.readLine()).startsWith("+OK"));
             do {
                 str = from.readLine();
-                if (str.compareTo("."
-                ) != 0) {
+                if (str.compareTo(".") != 0) {
                     v.add(str);
                 }
-            } while (str.compareTo("."
-            ) != 0);
-            for (int i = 0; i < v.s ize();
-            i++ 
-                )
- { 
-          title = (String) v.e
-                lementAt(i);
-                to.println("RETR " + (new StringTokenizer(title))
-                        .nextToken()
-                        + "\r");
-                while (!(from.readLine()).star
-
-                    tsWith("+OK"
-                    ) ); 
-          msg = "";
+            } while (str.compareTo(".") != 0);
+            for (int i = 0; i < v.size(); i++) {
+                title = (String) v.elementAt(i);
+                to.println("RETR " + (new StringTokenizer(title)).nextToken() + "\r");
+                while (!(from.readLine()).startsWith("+OK"));
+                msg = "";
                 do {
-                    msg += from.readLine()
-                            + "\n";
-                } while (!msg.endsWith("\n.\n"
-                ));
+                    msg += from.readLine() + "\n";
+                } while (!msg.endsWith("\n.\n"));
                 System.out.println(msg);
             }
-            socket.
-                    close();
+            socket.close();
         } catch (Exception e) {
             System.err.println(e);
         }
